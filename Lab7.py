805ec1371a8dcd56230ed881cf8eeaeaa297d654
unknown <ben.delisle61@gmail.com>
2019-10-26 17:50:47 -0400

    The ball now bounces around the GFX Hat properly, code more complete

--- Lab7.py
+++ Lab7.py
@@ -14,68 +14,6 @@ ball =  [
 [0,0,0,1,1,0,0,0]
 ]
 
-def deleteObject(obj, x=0, y=0):
-    positionx = x
-    positiony = y
-    for i in obj:
-        for j in i:
-            if j == 1:
-                lcd.set_pixel(positionx, positiony, 0)
-            positionx += 1
-        positiony+=1
-        positionx = x
-
-def moveObject(obj, x=0, y=0, vx=0, vy=0):
-    x = x + vx
-    y = y + vy
-    return x
-    return y
-
-def checkCollision(obj, x=0, y=0, vx=0, vy=0, Sx=128, Sy=64):
-    positionx = x
-    positiony = y
-
-    if vx < 0:
-        Sx = 0-1
-    if vy < 0:
-        Sy = 0-1
-
-    for i in obj:
-        if positiony == Sy:
-            vy = 0 - vy
-            y = y + vy
-            break
-        #only serves as a check if vertical speed is negative
-
-        for j in i:
-            if positionx == Sx:
-                break
-            #serves as a check if horizontal speed is negative
-
-            positionx += 1
-            if positionx == Sx:
-                break
-            #only breaks from the inside for loop, positive horizontal speed check
-
-        positiony+=1
-        if positionx == Sx:
-            vx = 0 - vx
-            x = x + vx
-            break
-        #sets the new horizontal speed and x position if needed
-
-        if positiony == Sy:
-            vy = 0 - vy
-            y = y + vy
-            break
-        positionx = x
-        #sets the new vertical speed and y position if needed
-    
-    return x
-    return y
-    return vx
-    return vy
-
 def displayObject(obj, x, y):
     positionx = x
     positiony = y
@@ -84,9 +22,75 @@ def displayObject(obj, x, y):
             if j == 1:
                 lcd.set_pixel(positionx, positiony, 1)
             positionx += 1
+            if positionx > 127:
+                break
         positiony+=1
+        if positiony > 63:
+            break
         positionx = x
     lcd.show()
 
-displayObject(ball, 0, 0)
+def deleteObject(obj, x=0, y=0):
+    positionx = x
+    positiony = y
+    for i in obj:
+        for j in i:
+            if j == 1:
+                lcd.set_pixel(positionx, positiony, 0)
+            positionx += 1
+            if positionx > 127:
+                break
+        positiony+=1
+        if positiony > 63:
+            break
+        positionx = x
+    lcd.show()
 
+def moveObject(obj, x=0, y=0, vx=0, vy=0):
+    x = x + vx
+    y = y + vy    
+    positionx = x
+    positiony = y
+    displayObject(obj, positionx, positiony)
+    return x, y
+
+def checkCollision(obj, x=0, y=0, vx=0, vy=0, Sx=128, Sy=64):
+    nextPositionx = x + vx
+    nextPositiony = y + vy
+    nextPositionEndx = x + vx + len(obj[0])
+    nextPositionEndy = y + vy + len(obj)
+    limitx = Sx
+    limity = Sy
+
+    if nextPositionEndx == limitx:
+        vx = vx * -1
+    elif nextPositionx < 0:
+        vx = vx * -1
+    
+    if nextPositionEndy == limity:
+        vy = vy * -1
+    elif nextPositiony < 0: 
+        vy = vy * -1
+
+    return vx, vy
+
+print("Look at the GFX Hat and you will see a ball bouncing around the screen.")
+print("Press Ctrl C to stop the program.")
+
+xValue = 0
+yValue = 0
+xSpeed = 1
+ySpeed = 1
+
+while True:
+    deleteObject(ball, x=xValue, y=yValue)
+
+    moveObject(ball, x=xValue, y=yValue, vx=xSpeed, vy=ySpeed)
+
+    moveReturn = moveObject(ball, x=xValue, y=yValue, vx=xSpeed, vy=ySpeed)
+    xValue = moveReturn[0]
+    yValue = moveReturn[1]
+
+    newSpeeds = checkCollision(ball, x=xValue, y=yValue, vx=xSpeed, vy=ySpeed)
+    xSpeed = newSpeeds[0]
+    ySpeed = newSpeeds[1]
